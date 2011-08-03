05a45772a9c3a6d2c0034a7467ef0dd7be84a690
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2011-08-03 17:00:50 +0200

    implement basic options (play/pause/stop/next/prev)

--- lib/mpc.rb
+++ lib/mpc.rb
@@ -1,3 +1,55 @@
-module Mpc
-  # Your code goes here...
+require "socket"
+class Mpc
+
+  @@regexps = {
+    "ACK"  => /\AACK \[(\d+)\@(\d+)\] \{(.*)\} (.+)\Z/,
+    "OK"   => /\AOK\n\Z/,
+    "FILE" => /\Afile\:(.*)\Z/,
+  }
+
+  def initialize(host = "127.0.0.1", port = 6600)
+    @socket = TCPSocket.new(host, port)
+    @socket.gets
+  end
+
+  def play
+    send_command "play"
+  end
+
+  def stop
+    send_command "stop"
+  end
+
+  def pause
+    send_command "pause 1"
+  end
+
+  def next
+    send_command "next"
+  end
+
+  def previous
+    send_command "previous"
+  end
+
+  private
+
+  def send_command(command)
+    @socket.puts(command)
+    get_response
+  end
+
+  def get_response
+    response = ""
+    while line = @socket.gets do
+      if @@regexps["OK"].match(line)
+        return response
+      elsif error = @@regexps["ACK"].match(line)
+        raise ArgumentError.new(line)
+      else
+        response << line
+      end
+    end
+    response
+  end
 end
--- test/mpc_test.rb
+++ test/mpc_test.rb
@@ -6,4 +6,16 @@ class MpcTest < MiniTest::Unit::TestCase
     TCPSocket.any_instance.stubs(:puts).returns(nil)    
     @mpc = Mpc.new
   end
+
+  def test_gets_raises_an_exception_on_ACK_response
+    TCPSocket.any_instance.stubs(:gets).returns("ACK [5@0] {} unknown command \"asd\"\n")
+    assert_raises(ArgumentError){
+      @mpc.send(:send_command, "asd")
+    }
+  end
+
+  def test_gets_outputs_empty_string_on_OK_response
+    TCPSocket.any_instance.stubs(:gets).returns("OK\n")
+    assert_equal("", @mpc.stop )
+  end
 end
